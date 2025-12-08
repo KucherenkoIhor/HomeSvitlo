8550e081a83fc95ea969182c742234a1547e1b60
kuchernkoigor@gmail.com <>
2025-12-08 13:26:27 +0200

    [TECH] Fix iOS credentials security - use gitignored SecretConfig
    - Add SecretConfig.kt to .gitignore
    - Create SecretConfig.kt.template as reference
    - AppConfig.ios.kt now reads from SecretConfig

--- composeApp/src/iosMain/kotlin/com/home/svitlo/config/AppConfig.ios.kt
+++ composeApp/src/iosMain/kotlin/com/home/svitlo/config/AppConfig.ios.kt
@@ -1,9 +1,7 @@
 package com.home.svitlo.config
 
 actual object AppConfig {
-    // For iOS, these values need to be set during build or from Info.plist
-    // For now, using the same values - in production, use proper iOS configuration
-    actual val wifiSn: String = "SN6MBN9GUZ"
-    actual val tokenId: String = "20251208145659068702972"
+    actual val wifiSn: String = SecretConfig.wifiSn
+    actual val tokenId: String = SecretConfig.tokenId
 }
 
