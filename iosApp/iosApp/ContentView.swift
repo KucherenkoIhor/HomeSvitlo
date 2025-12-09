e91d7541cb6d753746b0e7ee84f9b0808a2a7a09
kuchernkoigor@gmail.com <>
2025-12-09 11:58:05 +0200

    Removes logs

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,28 +2,9 @@ import SwiftUI
 import ComposeApp
 
 struct ContentView: View {
-    @State private var showDebug = false
-    
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            ComposeView()
-                .ignoresSafeArea(.all)
-            
-            // Debug button (small, in corner)
-            Button(action: { showDebug = true }) {
-                Image(systemName: "ladybug")
-                    .font(.system(size: 16))
-                    .foregroundColor(.gray)
-                    .padding(8)
-                    .background(Color.white.opacity(0.8))
-                    .clipShape(Circle())
-            }
-            .padding(.top, 50)
-            .padding(.trailing, 16)
-        }
-        .sheet(isPresented: $showDebug) {
-            DebugView()
-        }
+        ComposeView()
+            .ignoresSafeArea(.all)
     }
 }
 
